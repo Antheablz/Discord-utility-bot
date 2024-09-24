0b5d7a13fb6644780ce039dc5b49ed36a8c50f29
Antheablz <agblais@icloud.com>
2024-09-24 12:25:03 -0700

    reformatting with black

--- main.py
+++ main.py
@@ -5,51 +5,45 @@ from discord import Colour
 from discord.ext import commands
 from dotenv import load_dotenv
 
-intents = discord.Intents.default() # or .all() if you ticked all, that is easier
-intents.message_content = True 
+intents = discord.Intents.default()  # or .all() if you ticked all, that is easier
+intents.message_content = True
 intents.members = True
 
-#bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 load_dotenv()
-token = os.getenv('TOKEN')
-#client = discord.Client(intents=intents)
+token = os.getenv("TOKEN")
 
-#@client.event
+
 @bot.event
 async def on_ready():
-     print("logged in as a bot {0.user}".format(bot))
+    print("logged in as a bot {0.user}".format(bot))
 
-    #elif "$message" in message.content:
-    #    randomNum = random.randint(0,4)
-    #    if randomNum == 1:
-    #        await message.channel.send("message 1")
-    #    elif randomNum == 2:
-    #        await message.channel.send("message 2")
-    #    elif randomNum == 3:
-    #        await message.channel.send("message 3")
 
 @bot.command()
 async def menu(ctx):
-    myColour = random.randint(0,0xFFFFFF)
+    myColour = random.randint(0, 0xFFFFFF)
     msg = discord.Embed(
         title="List of Commands: \n $commandList \n $hello \n $coinFlip \n $reactionRoles",
-        colour=myColour)
+        colour=myColour,
+    )
 
-    await ctx.send(embed = msg)
+    await ctx.send(embed=msg)
+
 
 @bot.command()
 async def reactionRoles(ctx):
-    myColour = random.randint(0,0xFFFFFF)
+    myColour = random.randint(0, 0xFFFFFF)
     embed = discord.Embed(
-        title="WELCOME!: \n", 
+        title="WELCOME!: \n",
         colour=myColour,
-        description="click the heart for a random colour role!")
+        description="click the heart for a random colour role!",
+    )
 
-    msg = await ctx.send(embed = embed)
+    msg = await ctx.send(embed=embed)
 
-    await msg.add_reaction('💕')
+    await msg.add_reaction("💕")
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -57,9 +51,8 @@ async def on_raw_reaction_add(payload):
     if payload.guild_id != guild.id:
         return
     if payload.emoji.name == "💕":
-        role = discord.utils.get(payload.member.guild.roles, name = "BIG BOOTY")
+        role = discord.utils.get(payload.member.guild.roles, name="BIG BOOTY")
         await payload.member.add_roles(role)
-
 
 
 @bot.event
@@ -70,37 +63,35 @@ async def on_raw_reaction_remove(payload):
     if payload.emoji.name == "💕":
         guild = await bot.fetch_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
-        role = discord.utils.get(guild.roles, name = "BIG WINNER")
+        role = discord.utils.get(guild.roles, name="BIG WINNER")
         await member.remove_roles(role)
 
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello!!')
+    await ctx.send("Hello!!")
+
 
 @bot.command()
 async def coinFlip(ctx):
-    listOne = ["heads","tails"]
+    listOne = ["heads", "tails"]
     coinNum = random.choice(listOne)
 
     await ctx.send("heads or tails?")
+
     def check(ctx):
         return (ctx.content == "heads" or ctx.content == "tails") and ctx.channel
-    
-    msg = await bot.wait_for('message',check=check)
+
+    msg = await bot.wait_for("message", check=check)
     if msg.content == coinNum:
         await ctx.send("congrats you WON!")
     elif msg.content != coinNum:
-        await ctx.send("sorry you LOST! \n your guess: " + msg.content + "\n the flip was: " + coinNum)
+        await ctx.send(
+            "sorry you LOST! \n your guess: "
+            + msg.content
+            + "\n the flip was: "
+            + coinNum
+        )
 
-        
+
 bot.run(token)
-
-#client.run(token)
-
-
-
-
-
-
-
